f0c3d6e3c11c95272e7f63cdb231e76d3916ea57
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2022-06-25 01:04:30 +0100

    Add TOF view options

--- iocBoot/iocGP2CameraTest/st.cmd
+++ iocBoot/iocGP2CameraTest/st.cmd
@@ -20,12 +20,18 @@ GP2CameraTest_registerRecordDeviceDriver pdbbase
 epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 asynSetMinTimerPeriod(0.001)
 
-NetShrVarConfigure("nsv", "sec1", "$(TOP)/GP2CameraTestApp/src/netvarconfig.xml", 50, 0)
-## pass 1 as 4th argument to do callbacks on PV with array data
-GP2CameraConfigure("gp2", "nsv", "DATA", 1)
-#GP2CameraConfigure("gp2", "nsv", "DATA", 0)
+# This waveform allows transporting 8-bit images
+# needs to fit in EPICS_CA_MAX_ARRAY_BYTES
+epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES",  "20000000")
+#
 
-dbLoadRecords("$(TOP)/db/ADGP2Camera.template","P=$(MYPVPREFIX),R=GP2:,PORT=gp2,ADDR=0,TIMEOUT=1,ENABLED=1,DATATYPE=2,TYPE=Int16")
+NetShrVarConfigure("nsv", "sec1", "$(TOP)/GP2CameraTestApp/src/netvarconfig.xml", 50, 0)
+## pass 1 as 4th argument to do callbacks on PV with NSV array data
+#GP2CameraConfigure("gp2", "nsv", "DATA", 1, 10)
+GP2CameraConfigure("gp2", "nsv", "DATA", 0, 10)
+
+dbLoadRecords("$(TOP)/db/ADGP2Camera.template","P=$(MYPVPREFIX),R=GP2:,PORT=gp2,ADDR=0,TIMEOUT=1,ENABLED=1,DATATYPE=4,TYPE=Int32")
+dbLoadRecords("$(TOP)/db/ADGP2Camera.template","P=$(MYPVPREFIX),R=GP2:TOF:,PORT=gp2,ADDR=1,TIMEOUT=1,ENABLED=1,DATATYPE=4,TYPE=Int32")
 
 ## Load record instances
 
@@ -36,38 +42,32 @@ dbLoadRecords("$(TOP)/db/ADGP2Camera.template","P=$(MYPVPREFIX),R=GP2:,PORT=gp2,
 dbLoadRecords("db/TestGP2Camera.db","P=$(MYPVPREFIX)GP2:")
 dbLoadRecords("db/GP2extra.db","P=$(MYPVPREFIX)GP2:")
 
-NDStdArraysConfigure("Image1", 3, 0, "gp2", 0, 0)
-NDStdArraysConfigure("Image2", 3, 0, "gp2", 1, 0)
+NDTransformConfigure("RIMAGE1", 3, 0, "gp2", 0, 0)
+NDTransformConfigure("RIMAGE2", 3, 0, "gp2", 1, 0)
+dbLoadRecords("NDTransform.template", "P=$(MYPVPREFIX),R=GP2:RIMAGE1:,PORT=RIMAGE1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=gp2,NDARRAY_ADDR=0,DATATYPE=4,ENABLED=1,TYPE=Int32,FTVL=LONG,NELEMENTS=150000")
+dbLoadRecords("NDTransform.template", "P=$(MYPVPREFIX),R=GP2:RIMAGE2:,PORT=RIMAGE2,ADDR=0,TIMEOUT=1,NDARRAY_PORT=gp2,NDARRAY_ADDR=1,DATATYPE=4,ENABLED=1,TYPE=Int32,FTVL=LONG,NELEMENTS=150000")
 
-# This waveform allows transporting 8-bit images
-# needs to fit in EPICS_CA_MAX_ARRAY_BYTES
-epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES",  "20000000")
-#
-dbLoadRecords("NDStdArrays.template", "P=$(MYPVPREFIX),R=GP2:image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=gp2,NDARRAY_ADDR=0,TYPE=Int16,FTVL=SHORT,NELEMENTS=150000,ENABLED=1,DATATYPE=2")
-dbLoadRecords("NDStdArrays.template", "P=$(MYPVPREFIX),R=GP2:image2:,PORT=Image2,ADDR=0,TIMEOUT=1,NDARRAY_PORT=gp2,NDARRAY_ADDR=1,TYPE=Int16,FTVL=SHORT,NELEMENTS=150000,ENABLED=1,DATATYPE=2")
-
-NDProcessConfigure("PROC1", 3, 0, "gp2", 0)
-dbLoadRecords("NDProcess.template", "P=$(MYPVPREFIX),R=GP2:PROC1:,PORT=PROC1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=gp2,ENABLED=1,DATATYPE=2,TYPE=Int16,FTVL=SHORT,NELEMENTS=150000")
-
-NDTransformConfigure("TPROC1", 3, 0, "PROC1", 0, 0)
-dbLoadRecords("NDTransform.template", "P=$(MYPVPREFIX),R=GP2:TPROC1:,PORT=TPROC1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=PROC1,NDARRAY_ADDR=0,DATATYPE=4,ENABLED=1,TYPE=Int32,FTVL=LONG,NELEMENTS=150000")
-
-NDROIConfigure("ROI1", 3, 0, "TPROC1", 0, 0)
-dbLoadRecords("NDROI.template", "P=$(MYPVPREFIX),R=GP2:roi1:,PORT=ROI1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TPROC1,NDARRAY_ADDR=0,DATATYPE=4,ENABLED=1,TYPE=Int32,FTVL=LONG,NELEMENTS=150000")
+NDROIConfigure("ROI1", 3, 0, "RIMAGE1", 0, 0)
+NDROIConfigure("ROI2", 3, 0, "RIMAGE2", 0, 0)
+dbLoadRecords("NDROI.template", "P=$(MYPVPREFIX),R=GP2:ROI1:,PORT=ROI1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=RIMAGE1,NDARRAY_ADDR=0,DATATYPE=4,ENABLED=1,TYPE=Int32,FTVL=LONG,NELEMENTS=150000")
+dbLoadRecords("NDROI.template", "P=$(MYPVPREFIX),R=GP2:ROI2:,PORT=ROI2,ADDR=0,TIMEOUT=1,NDARRAY_PORT=RIMAGE2,NDARRAY_ADDR=0,DATATYPE=4,ENABLED=1,TYPE=Int32,FTVL=LONG,NELEMENTS=150000")
 
 NDStatsConfigure("STATS1", 3, 0, "ROI1", 0)
+NDStatsConfigure("STATS2", 3, 0, "ROI2", 0)
 NDTimeSeriesConfigure("STATS1_TS", 100, 0, "ROI1", 0, 22, 0, 0, 0, 0)
+NDTimeSeriesConfigure("STATS2_TS", 100, 0, "ROI2", 0, 22, 0, 0, 0, 0)
 dbLoadRecords("NDStats.template", "P=$(MYPVPREFIX),R=GP2:STATS1:,PORT=STATS1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=ROI1,ENABLED=1,DATATYPE=4,TYPE=Int32,FTVL=LONG,NCHANS=1,XSIZE=1,YSIZE=1,HIST_SIZE=1")
+dbLoadRecords("NDStats.template", "P=$(MYPVPREFIX),R=GP2:STATS2:,PORT=STATS2,ADDR=0,TIMEOUT=1,NDARRAY_PORT=ROI2,ENABLED=1,DATATYPE=4,TYPE=Int32,FTVL=LONG,NCHANS=1,XSIZE=1,YSIZE=1,HIST_SIZE=1")
 
+NDStdArraysConfigure("IMAGE1", 3, 0, "ROI1", 0, 0)
+NDStdArraysConfigure("IMAGE2", 3, 0, "ROI2", 0, 0)
+dbLoadRecords("NDStdArrays.template", "P=$(MYPVPREFIX),R=GP2:IMAGE1:,PORT=IMAGE1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=ROI1,NDARRAY_ADDR=0,TYPE=Int32,FTVL=LONG,NELEMENTS=150000,ENABLED=1,DATATYPE=4")
+dbLoadRecords("NDStdArrays.template", "P=$(MYPVPREFIX),R=GP2:IMAGE2:,PORT=IMAGE2,ADDR=0,TIMEOUT=1,NDARRAY_PORT=ROI2,NDARRAY_ADDR=0,TYPE=Int32,FTVL=LONG,NELEMENTS=150000,ENABLED=1,DATATYPE=4")
 
-#NDColorConvertConfigure("COL", 3, 0, "PROC", 0)
-#dbLoadRecords("NDColorConvert.template","P=$(MYPVPREFIX),R=GP2:COL:,PORT=COL,ADDR=0,TIMEOUT=1,NDARRAY_PORT=PROC,ENABLED=1")  
-
-NDPvaConfigure("PVA1", 3, 0, "ROI1", 0, "$(MYPVPREFIX)GP2:V4:image")
-dbLoadRecords("NDPva.template", "P=$(MYPVPREFIX),R=GP2:V4:,PORT=PVA1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=ROI1,ENABLED=1,DATATYPE=4")
-
-NDStdArraysConfigure("ImageSum", 3, 0, "ROI1", 0, 0)
-dbLoadRecords("NDStdArrays.template", "P=$(MYPVPREFIX),R=GP2:imageSum:,PORT=ImageSum,ADDR=0,TIMEOUT=1,NDARRAY_PORT=ROI1,TYPE=Int32,FTVL=LONG,NELEMENTS=150000,ENABLED=1,DATATYPE=4")
+NDPvaConfigure("PVA1", 3, 0, "ROI1", 0, "$(MYPVPREFIX)GP2:V4:IMAGE1:DATA")
+NDPvaConfigure("PVA2", 3, 0, "ROI2", 0, "$(MYPVPREFIX)GP2:V4:IMAGE2:DATA")
+dbLoadRecords("NDPva.template", "P=$(MYPVPREFIX),R=GP2:V4:IMAGE1,PORT=PVA1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=ROI1,ENABLED=1,DATATYPE=4")
+dbLoadRecords("NDPva.template", "P=$(MYPVPREFIX),R=GP2:V4:IMAGE2,PORT=PVA2,ADDR=0,TIMEOUT=1,NDARRAY_PORT=ROI2,ENABLED=1,DATATYPE=4")
 
 #NDFileHDF5Configure ("HDF5", 3, 0, "gp2", 0)
 #dbLoadRecords("NDFileHDF5.template", "P=$(MYPVPREFIX),R=GP2:HDF5:,PORT=HDF5,ADDR=0,TIMEOUT=1,NDARRAY_PORT=gp2,ENABLED=1")
@@ -86,9 +86,3 @@ iocInit
 
 ##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
 < $(IOCSTARTUP)/postiocinit.cmd
-
-dbpf "$(MYPVPREFIX)GP2:PROC1:FilterType", "Sum"
-dbpf "$(MYPVPREFIX)GP2:PROC1:NumFilter", "1000000000"
-dbpf "$(MYPVPREFIX)GP2:PROC1:EnableFilter", "1"
-dbpf "$(MYPVPREFIX)GP2:PROC1:DataTypeOut", "Int32"
-dbpf "$(MYPVPREFIX)GP2:DataType", "2"
